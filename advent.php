e59c7a17a4691653e40d99f85e2f99d98fd75d83
florianlangard <florian.langard@gmail.com>
2022-12-04 12:15:45 +0100

    solved

--- advent.php
+++ advent.php
@@ -1,5 +1,24 @@
 <?php
 
+$priorityList = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 $raw = file_get_contents('data.txt');
-$data = str_replace("\n\n"," ",$raw);
-$data = explode(" ", $data);
+$data = explode("\n", $raw);
+
+foreach ($data as $rucksack) {
+    $orderedRucksack[] = str_split($rucksack, (strlen($rucksack)/2));
+}
+
+foreach ($orderedRucksack as $compartment) {
+    $singleValues0 = str_split($compartment[0]);
+    $singleValues1 = str_split($compartment[1]);
+    $duplicatedItems[] = array_intersect($singleValues0, $singleValues1);
+}
+
+$result = 0;
+foreach ($duplicatedItems as $foundItem) {
+    $firstKey = array_key_first($foundItem);
+    $singlePriority = $foundItem[$firstKey];
+    $result += (strpos($priorityList, $singlePriority) + 1);
+}
+var_dump($result);
